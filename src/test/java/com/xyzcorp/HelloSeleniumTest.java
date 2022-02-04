b63e157492b336ad6b5d3929d010cd1d1af8d6af
Yannick Nzoda Situ <yannick.nzodasitu@cgi.com>
2022-02-04 16:55:16 -0500

    add pagetest

--- src/test/java/com/xyzcorp/HelloSeleniumTest.java
+++ src/test/java/com/xyzcorp/HelloSeleniumTest.java
@@ -5,29 +5,32 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HelloSeleniumTest {
 
-    private ChromeDriver chromeDriver;
+    private static ChromeDriver chromeDriver;
 
     @BeforeAll
-    public void setup() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\yannick.nzodasitu\\chromedriver_win32\\98\\chromedriver");
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\yannick.nzodasitu\\chromedriver_win32\\98\\chromedriver.exe");
         chromeDriver = new ChromeDriver();
     }
 
     @Test
     public void testpage() {
 
-        chromeDriver.get("https://www.google.com/search?q=Behavior-driven&rlz=1C1GCEB_enCA988CA988&oq=beh&aqs=chrome.0.69i59j69i57j69i61.2575j0j4&sourceid=chrome&ie=UTF-8");
-        chromeDriver.manage().window().setSize(new Dimension(1920, 1040));
-        chromeDriver.findElement(By.cssSelector(".CCgQ5 > span")).click();
-        chromeDriver.findElement(By.cssSelector(".css-steigm-MainContent > div > .css-5ph0o-Mask > .css-mf0yu5-ContentWrapper")).click();
+        chromeDriver.get("https://www.google.com/");
+        chromeDriver.manage().window().setSize(new Dimension(1758, 1070));
+        chromeDriver.findElement(By.name("q")).click();
+        chromeDriver.findElement(By.name("q")).sendKeys("Behavior Driven Development");
+        chromeDriver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+
     }
 
     @AfterAll
-    public void cleanup() {
+    public static void cleanup() {
         chromeDriver.close();
     }
 
